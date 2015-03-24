387b3b0e37dc16e22da217ee4e422e1fc1009854
ZA <ZA@10.16.156.182>
2015-03-24 18:55:40 +0100

    Zohra rocks!!!!!

--- CDIO-2/src/cdio/pkg2/CDIO2.java
+++ CDIO-2/src/cdio/pkg2/CDIO2.java
@@ -15,7 +15,7 @@ public class CDIO2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        // TODO code application logic her//
+    	// Zohra er sej!!!
     
 }
